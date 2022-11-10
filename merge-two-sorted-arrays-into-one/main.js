461be96bb5f28974bd55069b247c5421f12ec6e3
Laura Oprisan <99084293+lauraoprisan@users.noreply.github.com>
2022-11-11 00:09:05 +0200

    Normal practice

--- merge-two-sorted-arrays-into-one/main.js
+++ merge-two-sorted-arrays-into-one/main.js
@@ -1,10 +1,6 @@
 function mergeArrays(arr1, arr2) {
     arr2.forEach(item=> arr1.push(item) )
-    console.log(arr1)
     arr1.sort((a,b)=>a-b)
-    console.log(arr1)
-    let unique = [...new Set(arr1)]
-    console.log(unique)
+    let mergedArrays = [...new Set(arr1)]
+    return mergedArrays
   }
-
-  mergeArrays([1,2,4],[2,3,5])
